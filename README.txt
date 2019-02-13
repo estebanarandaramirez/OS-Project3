c16360ac57fd4f916336759ff2cd21878f1b48ae
estebanarandaramirez <44984627+estebanarandaramirez@users.noreply.github.com>
2019-02-12 23:49:08 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+OS - PROJECT 3 
+Esteban Aranda (earandaramirez) & Nicholas Hollan (nchollan)
+------------------------------------------------------------
+Phase 1:
+Work in progress for Phase 1 of the project. Assumptions made: simulations can last up to 1440 seconds (24 hs in the simulation) and a pirate/ninja can return to the shop a maximum of 20 times. For the randomized times: in the case of arrival time, we are utilizing the user input as a base and adding a random number between 1 and 1440 to it. For costuming times we are using that as a base to create random numbers around that value. 
+To compile the project type "make" when in the appropriate directory. To run the program, the user has to input "./phase1_project3 #costumingteams #pirates #ninjas costumingTimePirates costumingTimeNinjas arrivalTimePirates arrivalTimeNinjas". For example:
+./phase1_project3 3 20 12 15 5 10 10
+To clean the project type "make clean".
+
--- phase1_project3.c
+++ phase1_project3.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <pthread.h>
@@ -10,41 +11,47 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-#define MAX_TIME 1440
-//make assumption about how many times a pirate/ninja can come back
-void singleDaySim(int customing, int pirates, int ninjas, float customingTimeP, float customingTimeN, float arrivalTimeP, float arrivalTimeN);
+#define MAX_TIME 1440 //number of seconds to simulate a day
+#define MAX_VISITS 20 //max number of visits possible
 
-struct pirate {
+struct costumingTeam {
   int id;
-	float waitTime;
-	float customingTime;
-	int visits;
-  struct pirate *next;
+  bool isFree;
+  float busyTime;
+  float freeTime;
 };
 
-struct ninja {
+struct customer {
+  int flag;   //0 for pirates, 1 for ninjas
   int id;
-	float waitTime;
-	float customingTime;
-	int visits;
-  struct ninja *next;
+  int visits;
+  pthread_t thread;
+  int costumingTeam[MAX_VISITS];
+	float waitTime[MAX_VISITS];
+	float costumingTime[MAX_VISITS];
+  struct customer *next;
 };
+
+struct customer* initializeCustomers(int costuming, int pirates, int ninjas, float costumingTimeP, float costumingTimeN, float arrivalTimeP, float arrivalTimeN);
+float randomizeArrivalTime(float upper);
+float randomizeCostumingTime(float upper);
+void singleDaySim(int costuming, int pirates, int ninjas, float costumingTimeP, float costumingTimeN, float arrivalTimeP, float arrivalTimeN, struct customer *customerHead, struct costumingTeam *arrayTeams);
 
 int main(int argc, char *argv[]) {
-	int customing, pirates, ninjas;
-	float customingTimeP, customingTimeN, arrivalTimeP, arrivalTimeN;
+	int costuming, pirates, ninjas;
+	float costumingTimeP, costumingTimeN, arrivalTimeP, arrivalTimeN;
 	unsigned int seed = 0;
 
 	if (argc != 8) {		//test to see if the command line has the correct number of arguments
-		printf("Usage: ./phase1_project3 #costumingteams #pirates #ninjas customingTimePirates customingTimeNinjas arrivalTimePirates arrivalTimeNinjas\n");
+		printf("Usage: ./phase1_project3 #costumingteams #pirates #ninjas costumingTimePirates costumingTimeNinjas arrivalTimePirates arrivalTimeNinjas\n");
 		return EXIT_FAILURE;
 	} else {
 		for (int i = 1; i < argc; i++){
 			switch(i){
 			case 1:
-				customing = atoi(argv[i]);
-        if((customing < 2) || (customing > 4)){
-          printf("# of customing teams has to be between 2 and 4\n");
+				costuming = atoi(argv[i]);
+        if((costuming < 2) || (costuming > 4)){
+          printf("# of costuming teams has to be between 2 and 4\n");
       		return EXIT_FAILURE;
         }
 				break;
@@ -63,24 +70,21 @@ int main(int argc, char *argv[]) {
         }
 				break;
 			case 4:
-				customingTimeP = atof(argv[i]);
-				printf("%f\n", customingTimeP);
-				if((customingTimeP < 1) || (customingTimeP > MAX_TIME)){
-					printf("Customing time for pirates has to be between 1 and 1440\n");
+				costumingTimeP = atof(argv[i]);
+				if((costumingTimeP < 1) || (costumingTimeP > MAX_TIME)){
+					printf("costuming time for pirates has to be between 1 and 1440\n");
 					return EXIT_FAILURE;
 				}
 				break;
 			case 5:
-				customingTimeN = atof(argv[i]);
-				printf("%f\n", customingTimeN);
-				if((customingTimeN < 1) || (customingTimeN > MAX_TIME)){
-					printf("Customing time for ninjas has to be between 1 and 1440\n");
+				costumingTimeN = atof(argv[i]);
+				if((costumingTimeN < 1) || (costumingTimeN > MAX_TIME)){
+					printf("costuming time for ninjas has to be between 1 and 1440\n");
 					return EXIT_FAILURE;
 				}
 				break;
 			case 6:
 				arrivalTimeP = atof(argv[i]);
-				printf("%f\n", arrivalTimeP);
 				if((arrivalTimeP < 1) || (arrivalTimeP > MAX_TIME)){
 					printf("Arrival time for pirates has to be between 1 and 1440\n");
 					return EXIT_FAILURE;
@@ -88,7 +92,6 @@ int main(int argc, char *argv[]) {
 				break;
 			case 7:
 				arrivalTimeN = atof(argv[i]);
-				printf("%f\n", arrivalTimeN);
 				if((arrivalTimeN < 1) || (arrivalTimeN > MAX_TIME)){
 					printf("Arrival time for ninjas has to be between 1 and 1440\n");
 					return EXIT_FAILURE;
@@ -103,32 +106,122 @@ int main(int argc, char *argv[]) {
 	else						//otherwise, initialize the random sequence with the value of seed
 		srand(seed);
 
-	singleDaySim(customing, pirates, ninjas, customingTimeP, customingTimeN, arrivalTimeP, arrivalTimeN);
+  struct costumingTeam* arrayTeams = malloc(costuming * sizeof(*arrayTeams));
+  for(int i = 1; i <= costuming; i++){
+    arrayTeams[i].id = i;
+    arrayTeams[i].isFree = true;
+  }
+
+  struct customer *customerHead;
+  customerHead = (struct customer*)malloc(sizeof(struct customer));
+  customerHead = initializeCustomers(costuming, pirates, ninjas, costumingTimeP, costumingTimeN, arrivalTimeP, arrivalTimeN);
+
+	singleDaySim(costuming, pirates, ninjas, costumingTimeP, costumingTimeN, arrivalTimeP, arrivalTimeN, customerHead, arrayTeams);
 	return EXIT_SUCCESS;
 }
 
-void singleDaySim(int customing, int pirates, int ninjas, float customingTimeP, float customingTimeN, float arrivalTimeP, float arrivalTimeN){
-	int numVisits;
-	float busyTimeTeam1, freeTimeTeam1, busyTimeTeam2, freeTimeTeam2;
-  if(customing == 3 || customing == 4){
-	 float busyTimeTeam3, freeTimeTeam3;
-  }
-  if(customing == 4){
-	 float busyTimeTeam4, freeTimeTeam4;
-  }
-	float averageQueueLength, grossRevenue, goldePerVisit, totalProfits;
+struct customer* initializeCustomers(int costuming, int pirates, int ninjas, float costumingTimeP, float costumingTimeN, float arrivalTimeP, float arrivalTimeN){
+  int counter = 1;
 
-	printf("Time customing team 1 was busy: %f\n", busyTimeTeam1);
-	printf("Time customing team 1 was free: %f\n", freeTimeTeam1);
-	printf("Time customing team 2 was busy: %f\n", busyTimeTeam2);
-	printf("Time customing team 2 was free: %f\n", freeTimeTeam2);
-  if(customing == 3 || customing == 4){
-		printf("Time customing team 3 was busy: %f\n", busyTimeTeam3);
-		printf("Time customing team 3 was free: %f\n", freeTimeTeam3);
+  struct customer *customerHead;
+  customerHead = (struct customer*)malloc(sizeof(struct customer));
+  customerHead->flag = 0;
+  customerHead->id = counter;
+  customerHead->visits = 1;
+  customerHead->costumingTeam[0] = 1;
+  customerHead->waitTime[0] = 0;
+  customerHead->costumingTime[0] = costumingTimeP;
+  customerHead->next = NULL;
+  counter++;
+
+  struct customer *temp1;
+  temp1 = (struct customer*)malloc(sizeof(struct customer));
+  temp1 = customerHead;
+  for(int i = 0; i < pirates; i++){
+    struct customer *temp2;
+    temp2 = (struct customer*)malloc(sizeof(struct customer));
+    temp2->flag = 0;
+    temp2->id = counter;
+    temp2->visits = 1;
+    temp2->costumingTeam[0] = 1;
+    temp2->waitTime[0] = 0;
+    temp2->costumingTime[0] = costumingTimeP;
+    temp2->next = NULL;
+    counter++;
+    temp1->next = temp2;
+    temp1 = temp2;
   }
-	if(customing == 4){
-		printf("Time customing team 4 was busy: %f\n", busyTimeTeam4);
-		printf("Time customing team 4 was free: %f\n", freeTimeTeam4);
+  for(int i = 0; i < ninjas; i++){
+    struct customer *temp2;
+    temp2 = (struct customer*)malloc(sizeof(struct customer));
+    temp2->flag = 1;
+    temp2->id = counter;
+    temp2->visits = 1;
+    temp2->costumingTeam[0] = 1;
+    temp2->waitTime[0] = 0;
+    temp2->costumingTime[0] = costumingTimeN;
+    temp2->next = NULL;
+    counter++;
+    temp1->next = temp2;
+    temp1 = temp2;
+  }
+  return customerHead;
+}
+
+float randomizeArrivalTime(float upper){
+  float average = (float)rand()/(float)(RAND_MAX/upper);
+  float randomTimeOfDay = (float)rand()/(float)(RAND_MAX/MAX_TIME);
+  float r = randomTimeOfDay + average;
+  return r;
+}
+
+float randomizeCostumingTime(float upper){
+  float r = (float)rand()/(float)(RAND_MAX/upper);
+  return r;
+}
+
+void singleDaySim(int costuming, int pirates, int ninjas, float costumingTimeP, float costumingTimeN, float arrivalTimeP, float arrivalTimeN, struct customer *customerHead, struct costumingTeam *arrayTeams){
+	int numVisits = 0;
+	float totalQueueLength = 0, averageQueueLength, grossRevenue = 0, goldePerVisit, totalProfits;
+
+
+  //Print statistics
+  for(int j = 0; j < (pirates+ninjas); j++){
+    float goldOwed = 0;
+    if(customerHead->flag == 0){
+      printf("Pirate #%d:\n", customerHead->id);
+    } else if(customerHead->flag == 1){
+      printf("Ninja #%d:\n", customerHead->id);
+    }
+    printf("Number of visits: %d\n", customerHead->visits);
+    for(int i = 0; i < customerHead->visits; i++){
+      printf("Visit #%d:\n", i+1);
+      printf("\tCostuming time: %f\n", customerHead->costumingTime[i]);
+      printf("\tWait time: %f\n", customerHead->waitTime[i]);
+      totalQueueLength += customerHead->waitTime[i];
+      if(customerHead->waitTime[i] <= 30){
+        goldOwed += customerHead->costumingTime[i];
+      }
+      arrayTeams[customerHead->costumingTeam[i]].busyTime +=  customerHead->costumingTime[i];
+    }
+    printf("Total gold owed: %f\n\n", goldOwed);
+    numVisits += customerHead->visits;
+    grossRevenue += goldOwed;
+    if(customerHead->next != NULL){
+      customerHead = customerHead->next;
+    } else {
+      printf("Out of bounds\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  averageQueueLength = (totalQueueLength)/(numVisits);
+  goldePerVisit = (grossRevenue)/(numVisits);
+  totalProfits = (grossRevenue)-(5*costuming);
+
+  for(int i = 1; i <= costuming; i++){
+    printf("Time costuming team %d was busy: %f\n", i, arrayTeams[i].busyTime);
+    printf("Time costuming team %d was free: %f\n", i, arrayTeams[i].freeTime);
   }
 	printf("Average queue length: %f\n", averageQueueLength);
 	printf("Gross revenue: %f\n", grossRevenue);
